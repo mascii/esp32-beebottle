3582f5fcb4880d35af4ceca97a283521cb3d2139
Masaki Koyanagi <mascii@gmail.com>
2018-05-13 11:54:41 +0900

    Use argv instead of process.argv

--- convcert.js
+++ convcert.js
@@ -13,9 +13,9 @@ function convcert(pem) {
 
 try {
   var argv = process.argv;
-  if (process.argv.length === 3) {
+  if (argv.length === 3) {
     var fs = require('fs');
-    var filename = process.argv[2];
+    var filename = argv[2];
     var pem = fs.readFileSync(filename, 'utf8').trim(); // String.prototype.trim(): ES2015
     var convertedPem = convcert(pem);
     fs.writeFileSync('./certificate.h', convertedPem);
